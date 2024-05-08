8d34f45cb4f87dc6611bcc2ef270d4cfaa727df1
TelesforoAleix <morenotelesforo.aleix@gmail.com>
2024-05-08 12:57:26 +0200

    added latest version of electron in -dev dependencies

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11dcb58dea34aa24afca1b39a015948bb2f1e06fac208014c3d30afcb70848d6
-size 1124
+oid sha256:3cd2a4de013832a47696fe6567372caff3af517c32c129b81533b874de4d49d8
+size 1127
